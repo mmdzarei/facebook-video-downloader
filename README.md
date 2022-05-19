3f466413181fc23f13d45d425d8c93ec24ebbb71
mmdzarei <36816900+mmdzarei@users.noreply.github.com>
2022-05-19 06:03:24 +0430

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# facebook-video-downloader-bot
+This bot gets a facebook link and uploads it to the user
+This is an old code, clean it up on your own
+
+Add telegram token to **line 33** add trusted users to **line 38**, and add admin(bot owner's telegram user_id to **line 94**)
+
+This was tested on python3.8
+
+The code is messy, use it on your own risk please.
+
+HD downloader does not work due to facebook xpath change.
+
+
---
+++ fb_downloader.py
@@ -0,0 +1,208 @@
+#change line 33,38,94
+#Add token to line 33 add trusted users to line 38, and add admin(bot owner's telegram user_id to line 94)
+
+import wget
+from lxml import html
+import requests
+import os
+import telebot
+
+
+import mechanize
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####################'''
+#bot
+TOKEN='*************************************'
+bot=telebot.TeleBot(TOKEN)
+
+
+#add trusted users
+users=['trusted_users1','trusted_users2','trusted_users3']
+global link0
+global hd_link
+
+link0=''
+hd_link=''
+'''#### rooye add user kar shavad
+
+users_text_file.writelines("%s" % i for i in users)
+users_text_file.close()
+users_text_file= open("users.txt","r+")
+useuse=users_text_file.readlines()
+print("useuse::\n",useuse)
+
+
+def read_users():
+	users=[]
+	users_text_file= open("users.txt","r")
+	for line in users_text_file:
+		line.rstrip("\n\r")
+		print(line)
+		users.append(line)
+	print(users)
+	return(users)
+
+users=read_users()
+
+def write_users(new_user):
+	users_text_file= open("users.txt","w")
+'''
+
+def dnld(link,name):
+	print('\nDownloading Started\n')
+	wget.download(link,name)
+	print('\nDownloaded\n')
+
+
+def get_fb_link(url0):
+	url=url0.replace('https://www','https://m')
+	page = requests.get(url0)
+	tree = html.fromstring(page.content)
+	##print("treeeeee\n\n\:\n",tree.xpath('//meta[10]'))
+	link = tree.xpath('//meta[@property="og:video:secure_url"]//@content')[0]
+	print('\n\n\n\n\nlink1:',link)
+
+
+
+	return(link)
+
+
+@ bot.message_handler(commands=['start','go'])
+def start_hanlder(message):
+	bot.send_message(message.chat.id,'Hello, Send me the facebook link, dude')
+
+@ bot.message_handler(commands=['add_user'])
+def add_user_hanlder(message):
+	if(message.from_user.username=='ADMIN_USERNAME'):  
+		txt="Hello, Admin. What's the user's username?"+"\n(Without "+"-@-"+")"
+		msg=bot.send_message(message.chat.id,txt)
+		bot.register_next_step_handler(msg,get_user)
+	else:
+	    bot.send_message(message.chat.id,"Only the admin can add users 😜")
+
+'''
+@ bot.message_handler
+def hd_hanlder(message):
+    if (message.text=="sd"):
+        print("\n\n\n\n\nYYYYEEESSS")
+    	#msg=bot.send_message(message.chat.id,"HD? or SD?")
+        bot.register_next_step_handler(message,get_hd_link)
+    	#print("HD link=\t",link0)
+'''
+
+def get_hd_link(message):
+    try:
+        if (message.text=="/sd"):
+            chat_id=message.chat.id
+            bot.send_message(message.chat.id,"Sending ...")
+            link=get_fb_link(link0)
+            bot.send_message(message.chat.id,link)                  #direct link
+            bot.send_message(message.chat.id,'Downloading')
+            video_name=str(chat_id)+'.mp4'
+            dnld(link,video_name)
+            bot.send_message(message.chat.id,'Downloaded')
+            video = open(video_name, 'rb')
+            bot.send_message(message.chat.id,'Uploading Video...')
+            bot.send_video(chat_id, video)
+            #file_id = 'AxxxxZZZzzz'
+            #bot.send_video(chat_id, file_id)
+            os.remove(video_name)                                #remove from server
+        elif (message.text=="/hd"):
+            chat_id=message.chat.id
+            bot.send_message(message.chat.id,"Sending ...")
+            page=requests.get(link0)
+            tree=html.fromstring(page.content)
+            #text = tree.xpath('//*[@id="facebook"]/body/script[10]/text()')
+            text = tree.xpath('/html/body/script[86]/text()')
+            s=text[0]
+            #print("\n\n\n\nS\s:",s)
+
+            start = 'hd_src:"'
+            end = '",sd_src'
+            hd_link=s[s.find(start)+len(start):s.rfind(end)]
+            bot.send_message(message.chat.id,hd_link)       #HD link
+            bot.send_message('@atron_fb_ch',hd_link)       #Send to channel
+            #print("HD Link====",hd_link)
+            #print("\n\n\n\n\n\n\n")
+            video_name=str(chat_id)+'.mp4'
+
+            ###########     Download and upload to telegram
+
+            #dnld(hd_link,video_name)
+            #video = open(video_name, 'rb')
+            #bot.send_video(chat_id, video)                 #upload video "file too big"
+            #os.remove(video_name)
+
+            return
+    except:
+        print(bot.send_message(message.chat.id,'Not downloadable or something else went wrong'))
+
+def get_user(message):
+	text=message.text.lower()
+	chat_id=message.chat.id
+	users.append(text)
+	print(users)
+	bot.send_message(message.chat.id,"Apended!")
+	return
+
+@ bot.message_handler(content_types=['text'])
+def text_handler(message):
+	global link0
+	global hd_link
+
+	text=message.text.lower()
+	link0=text
+	chat_id=message.chat.id
+	if (message.from_user.username in users):
+		if ('facebook.com' in text):
+			bot.send_message(message.chat.id,"/hd? or /sd?")
+			bot.register_next_step_handler(message,get_hd_link)
+
+
+		elif (text=='hello'):
+			print("ID==\t",message.from_user.id)
+			print("User_name==\t",message.from_user.username)
+			bot.send_message(message.chat.id,'Hello, Just send me the link, man')
+		else:
+			bot.send_message(message.chat.id,'da fuq? Send me a facebook video URL')
+
+	else:
+		bot.send_message(message.chat.id,"You're not a friend, only my friends are allowed to use me!")
+
+
+#bot.polling(none_stop=True)
+'''
+browser = mechanize.Browser()
+browser.set_handle_robots(False)
+cookies = mechanize.CookieJar()
+browser.set_cookiejar(cookies)
+browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7')]
+browser.set_handle_refresh(False)
+
+url = 'http://www.facebook.com/login.php'
+browser.open(url)
+browser.select_form(nr = 0)       #This is login-password form -> nr = number = 0
+browser.form['email'] = '*********@*****.com'
+browser.form['pass'] = '***********'
+response = browser.submit()
+print (response.read())
+'''
+bot.polling()
